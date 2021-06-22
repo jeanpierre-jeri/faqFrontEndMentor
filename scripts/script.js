7d16fe1d9deb521f67d5b43b8fddc4168c77d4cc
Jean Pierre <jeanpierre.webdev@gmail.com>
2021-06-21 22:07:34 -0500

    site deployment

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const accordion = document.querySelectorAll(".question")
+
+accordion.forEach((question) => question.addEventListener('click', () => {
+    
+    if(question.parentNode.classList.contains('active')) {
+        question.parentNode.classList.toggle('active')
+    } else {
+        accordion.forEach(question => question.parentNode.classList.remove('active'))
+
+        question.parentNode.classList.add('active')
+    }
+}))
